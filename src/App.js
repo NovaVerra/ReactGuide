3bd909a4e5c23131fe9801ffb448a3343193e0b6
lununglee <leo.lunung.lee@gmail.com>
2019-08-25 20:16:49 -0700

    R15 - Adding Two-Way Binding

--- src/App.js
+++ src/App.js
@@ -27,15 +27,35 @@ class App extends Component {
 		})
 	}
 
+	nameChangeHandler = (event) => {
+		this.setState({
+			persons: [
+				{name: "Abby", age: 19},
+				{name: event.target.value, age: 27},
+				{name: "Christina", age: 32}
+			]
+		})
+	}
+
 	render () {
 		return (
 			<div className="App">
 				<h1>Hello, I am an React App</h1>
 				<p>This is really working</p>
 				<button onClick={() => this.switchNameHandler("Zeta")}>Switch Name</button>
-				<Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-				<Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler.bind(this, "Unicorn")}>My Hobbies: Racing</Person>
-				<Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+				<Person
+					name={this.state.persons[0].name}
+					age={this.state.persons[0].age}/>
+				<Person
+					name={this.state.persons[1].name}
+					age={this.state.persons[1].age}
+					click={this.switchNameHandler.bind(this, "Unicorn")}
+					change={this.nameChangeHandler}>
+						My Hobbies: Racing
+					</Person>
+				<Person
+				name={this.state.persons[2].name}
+				age={this.state.persons[2].age}/>
 			</div>
 		)
 		// return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'))
