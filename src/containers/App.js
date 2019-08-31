3004afdd176fa7fb340231b2d176e3a0510404c9
lununglee <leo.lunung.lee@gmail.com>
2019-08-31 13:15:53 -0700

    R32 - Reorganizing Directory Layout

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import styles from './App.module.css'
-import Person from './Person/Person'
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary'
+import Person from '../components/Persons/Person/Person'
 
 class App extends Component {
 
@@ -59,13 +58,12 @@ class App extends Component {
 			persons = (
 				<div>
 					{this.state.persons.map((person, index) => {
-						return <ErrorBoundary key={person.id}>
-								<Person 
-								name={person.name}
-								age={person.age}
-								click={() => this.deletePersonHandler.bind(this, index)}
-								change={(event) => this.nameChangeHandler(event, person.id)}/>
-							</ErrorBoundary>
+						return <Person 
+							key={person.id}
+							name={person.name}
+							age={person.age}
+							click={() => this.deletePersonHandler.bind(this, index)}
+							change={(event) => this.nameChangeHandler(event, person.id)}/>
 					})}
 				</div>
 			)
