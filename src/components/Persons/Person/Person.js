abd13283aa911bf5971497157b44bdf81c8a385c
lununglee <leo.lunung.lee@gmail.com>
2019-09-06 21:26:59 -0700

    R47 - Passing Unknown Props

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react'
 import styles from './Person.module.css';
 import Aux from '../../../hoc/Aux'
+import withClass from '../../../hoc/withClass'
 
 class Person extends Component {
 	render () {
 		console.log("[Person.js] rendering...")
 		return (
 			<Aux>
-				<div className={styles.Person}>
-					<p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old</p>
-					<p>{this.props.children}</p>
-					<input type="text" onChange={this.props.change} value={this.props.name}/>
-				</div>
+				<p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old</p>
+				<p>{this.props.children}</p>
+				<input type="text" onChange={this.props.change} value={this.props.name}/>
 			</Aux>
 		)
 	}
 }
 
-export default Person
+export default withClass(Person, styles.Person)
