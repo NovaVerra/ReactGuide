62d9aad80110f39170b9f78a6bdd584b6a59fad4
lununglee <leo.lunung.lee@gmail.com>
2019-09-06 21:09:49 -0700

    R45 - Using Higher Order Component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react'
 import styles from './Person.module.css';
+import Aux from '../../../hoc/Aux'
 
 class Person extends Component {
 	render () {
 		console.log("[Person.js] rendering...")
 		return (
-			<div className={styles.Person}>
-				<p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old</p>
-				<p>{this.props.children}</p>
-				<input type="text" onChange={this.props.change} value={this.props.name}/>
-			</div>
+			<Aux>
+				<div className={styles.Person}>
+					<p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old</p>
+					<p>{this.props.children}</p>
+					<input type="text" onChange={this.props.change} value={this.props.name}/>
+				</div>
+			</Aux>
 		)
 	}
 }
