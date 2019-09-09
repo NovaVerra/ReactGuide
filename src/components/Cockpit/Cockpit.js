467a7e31cfc0cbbaffc7b5d0af968ed228892bec
lununglee <leo.lunung.lee@gmail.com>
2019-09-08 18:06:54 -0700

    R51 - Refs with React Hooks

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,29 +1,32 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import styles from './Cockpit.module.css'
 
 const Cockpit = (props) => {
 
+	const toggleButtonRef = useRef(null)
+	
 	useEffect(() => {
 		console.log("[Cockpit.js] useEffect")
-		// HTTP requests
-		setTimeout(() => {
-			alert("Saved data to cloud")
-		}, 1000)
+		// // HTTP requests
+		// setTimeout(() => {
+		// 	alert("Saved data to cloud")
+		// }, 1000)
+		toggleButtonRef.current.click()
 		return () => {
 			console.log("[Cockpit.js] cleanup work in useEffect")
 		}
 	}, [])
-
+	
 	useEffect(() => {
 		console.log("[Cockpit.js] 2nd useEffect")
 		return () => {
 			console.log("[Cockpit.js] cleanup work in 2nd useEffect")
 		}
 	})
-
+	
 	const classes = []
 	let btnStyle = ''
-
+	
 	if (props.showPersons) {
 		btnStyle = styles.Red
 	}
@@ -33,12 +36,13 @@ const Cockpit = (props) => {
 	if (props.personsLength <= 1) {
 		classes.push(styles.bold) // classes = ["red", "bold"]
 	}
-
+	
 	return (
 		<div className={styles.Cockpit}>
 			<h1>{props.title}</h1>
 			<p className={classes.join(' ')}>This is really working</p>
 			<button
+				ref={toggleButtonRef}
 				className={btnStyle} 
 				onClick={props.click}>Toggle Persons</button>
 		</div>
